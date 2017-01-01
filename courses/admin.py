6f54618d72be310a7b6214160beb51c8c57cb649
raajb <raaj@metabahn.com>
2016-12-31 19:20:00 -0600

    Added Hyperlinks and wrap-up

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Step
 # Register your models here.
 
-admin.site.register(Course)
+class StepInLine(admin.StackedInline):
+    model = Step
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [StepInLine,]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Step)
 
--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
   url(r'^$', views.course_list),
-
+  url(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.step_detail, 
+      name='step'),
+  url(r'(?P<pk>\d+)/$', views.course_detail, 
+      name='detail'),
 ]
